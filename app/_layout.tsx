9728b549423a678d72fde5320229b6d00fe84db6
Roger Paris <roger.parisvicent@gmail.com>
2025-05-01 12:52:56 +0200

    adds lib to make work uuid

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import { Slot } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { enableScreens } from 'react-native-screens';
+import 'react-native-get-random-values'
 
 enableScreens();
 
